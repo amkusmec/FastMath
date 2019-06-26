529392b553adeb97b1e9205f9499fcd1cb4b2071
Aaron Kusmec <amkusmec@iastate.edu>
2019-06-26 15:58:48 -0500

    Added some documentation and a README

--- R/FastMath.R
+++ R/FastMath.R
@@ -1,5 +1,16 @@
-## usethis namespace: start
+#' FastMath
+#' 
+#' C++ implementations of R functions for faster processing.
+#' 
+#' @description 
+#' This is an ad hoc package of numerical functions implemented using the C++
+#' Eigen matrix algebra library. These are primarily to speed up certain computations
+#' mostly pertaining to quantitative genetics applications for high-throughput
+#' genotyping data. The package will be updated as I find the need for more
+#' functions.
+#' 
+#' The package exposes both R and C++ interfaces for all included functions.
+#' 
 #' @import Rcpp
 #' @useDynLib FastMath, .registration = TRUE
-## usethis namespace: end
 NULL
